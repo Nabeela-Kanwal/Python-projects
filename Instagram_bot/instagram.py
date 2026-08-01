a612dc24acf9085dda3901925dfeaec5ea27e99d
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-08-01 21:24:30 +0500

    added instagram photo

--- Instagram_bot/instagram.py
+++ Instagram_bot/instagram.py
@@ -1,3 +1,5 @@
 from instabot import Bot
 bot = Bot()
 bot.login(username="codewithnabeela", password="nabeela7890")
+# bot.follow('wscubetechindia')
+bot.upload_photo("C:/Users/HP/Desktop/instagram bot/instabot/instagram.jpg", caption="This is a test photo upload using Instabot.")
