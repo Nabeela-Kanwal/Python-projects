5f592092cb7bc2957a2d983ce5db469a72a714e9
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-08-08 11:26:22 +0500

    comment out photo upload line in instagram bot script

--- Instagram_bot/instagram.py
+++ Instagram_bot/instagram.py
@@ -2,4 +2,5 @@ from instabot import Bot
 bot = Bot()
 bot.login(username="codewithnabeela", password="nabeela7890")
 # bot.follow('wscubetechindia')
-bot.upload_photo("C:/Users/HP/Desktop/instagram bot/instabot/instagram.jpg", caption="This is a test photo upload using Instabot.")  
+# bot.upload_photo("C:/Users/HP/Desktop/instagram bot/instabot/instagram.jpg", caption="This is a test photo upload using Instabot.")
+bot.unfollow('wscubetechindia')
